16829d4accaaa59dbec14758bb0e31c3e83febf2
Alexandra Chen <alechen04@gmail.com>
2015-08-11 20:02:54 +0200

    add admin role to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
   validates :email, presence: true, length: {maximum: 250}
   validates :full_name, presence: true, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ }
 end

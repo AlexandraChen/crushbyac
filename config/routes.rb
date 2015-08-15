b20ebbda3fe878ec6479e94ed05eea0ded0512c8
Alexandra Chen <alechen04@gmail.com>
2015-08-15 02:29:40 +0200

    logout link and admin test

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root "sites#index"
   devise_scope :user do
     get "/admin", to: "devise/sessions#new"
+    get "/logout", to: "devise/sessions#destroy", as: :signout
   end
 
   get "/admin/panel" => "users#admin_panel"

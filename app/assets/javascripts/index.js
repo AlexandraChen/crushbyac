5f8a69f9d950fb56f6071918d21bf9d09c89feba
Alexandra Chen <alechen04@gmail.com>
2015-08-18 14:52:43 -0500

    add location panel

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,25 +1,25 @@
-    $(document).ready(function () {
-      $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-          $('.scrollup').fadeIn()
-        } else {
-          $('.scrollup').fadeOut()
-        }
-      });
-      $('.scrollup').click(function () {
-        $("html, body").animate({
-          scrollTop: 0
-        }, 600)
-        return false
-      })
-      $('a[href^="#value-proposition"]').on('click',function (event) {
-        event.preventDefault()
-        var target = this.hash
-        var $target = $(target)
-        $('html, body').stop().animate({
-          'scrollTop': $target.offset().top
-        }, 900, 'swing', function () {
-          window.location.hash = target
-       })
-      })
-    })
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.scrollup').fadeIn()
+    } else {
+      $('.scrollup').fadeOut()
+    }
+  });
+  $('.scrollup').click(function () {
+    $("html, body").animate({
+      scrollTop: 0
+    }, 900)
+    return false
+  })
+  $('a[href^="#value-proposition"], a[href^="#location"]').on('click',function (event) {
+    event.preventDefault()
+    var target = this.hash
+    var $target = $(target)
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function () {
+      window.location.hash = target
+   })
+  })
+})

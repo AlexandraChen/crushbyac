bc1337674ff087f624531793d38bf77df88b0820
Alexandra Chen <alechen04@gmail.com>
2015-08-29 11:33:46 -0500

    redirect after deliver email

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -7,7 +7,7 @@ class ContactFormsController < ApplicationController
     @contact_form = ContactForm.new(params[:contact_form])
     @contact_form.request = request
     if @contact_form.deliver
-      redirect_to(root_path, :notice => "Message was successfully sent.")
+      redirect_to root_path, notice: "Message was successfully sent."
     else
       flash.now[:alert] = 'Please fill all the fields!'
     end

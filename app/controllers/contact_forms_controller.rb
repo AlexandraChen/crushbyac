383fc0c4f3b844ae5f2ad352ad0d18325d5449dd
Alexandra Chen <alechen04@gmail.com>
2015-08-29 11:24:28 -0500

    add smtp

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -4,13 +4,12 @@ class ContactFormsController < ApplicationController
   end
 
   def create
-      @contact_form = ContactForm.new(params[:contact_form])
-      @contact_form.request = request
-      if @contact_form.deliver
-        flash.now[:notice] = 'Thank you for your message!'
-      else
-        flash.now[:alert] = 'Please fill all the fields!'
-      end
-      redirect_to root_path
+    @contact_form = ContactForm.new(params[:contact_form])
+    @contact_form.request = request
+    if @contact_form.deliver
+      redirect_to(root_path, :notice => "Message was successfully sent.")
+    else
+      flash.now[:alert] = 'Please fill all the fields!'
+    end
   end
 end

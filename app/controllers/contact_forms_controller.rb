0f61a6c03b66362e6a194559e1cf28332eec701c
Alexandra Chen <alechen04@gmail.com>
2015-08-29 11:13:08 -0500

    redirect to root

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -8,9 +8,9 @@ class ContactFormsController < ApplicationController
       @contact_form.request = request
       if @contact_form.deliver
         flash.now[:notice] = 'Thank you for your message!'
-        redirect_to root_path
       else
         flash.now[:alert] = 'Please fill all the fields!'
       end
+      redirect_to root_path
   end
 end

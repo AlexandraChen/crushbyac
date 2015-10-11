c99bcb56b1c95ce089b393d2553e02f5943a274f
Alexandra Chen <alechen04@gmail.com>
2015-10-11 13:00:36 -0500

    reorder products

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,7 +1,14 @@
 class SitesController < ApplicationController
 	def index
-    @main_products = Product.all.order("created_at DESC").limit(3)
-    @products = Product.all.order("created_at ASC").limit(Product.count - 3).shuffle[0..Product.all.size]
+    # To show main products at the top
+    #@main_products = Product.all.order("created_at DESC").limit(3)
+    #Product.all.order("created_at ASC").limit(Product.count - 3).shuffle[0..Product.all.size]
+    @products = []
+    Product.all.order("created_at ASC").each do |x|
+      if x.published == true
+        @products << x
+      end
+    end
     @contact_form = ContactForm.new
 	end
 

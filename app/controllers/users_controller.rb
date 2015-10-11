9ad20e24f40dcfb5174a33769040f3ff35489086
Alexandra Chen <alechen04@gmail.com>
2015-10-11 14:42:26 -0500

    reorder as updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
 	def admin_panel
 		if current_user.admin?
 			@admin = current_user
+      @products = @admin.products.order("updated_at DESC")
 		else
 			render root_path
 		end

ce88866ec20e1024ec7a6a12e4c8b6876ab2f048
Alexandra Chen <alechen04@gmail.com>
2015-10-11 13:00:50 -0500

    show only published after search

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -2,9 +2,13 @@ class Api::ProductsController < ApplicationController
   respond_to :json
 
   def index
-    @products = Product.all
     if params[:search]
-      @products = Product.search(params[:search]).all
+      @products = []
+      Product.search(params[:search]).all.each do |x|
+        if x.published == true
+          @products << x
+        end
+      end
     end
     render :index
   end

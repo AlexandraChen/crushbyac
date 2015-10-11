b3f081d825c3ea484d5077738f50a5e53c0d4495
Alexandra Chen <alechen04@gmail.com>
2015-10-11 13:09:49 -0500

    reorder products DESC

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -4,7 +4,7 @@ class SitesController < ApplicationController
     #@main_products = Product.all.order("created_at DESC").limit(3)
     #Product.all.order("created_at ASC").limit(Product.count - 3).shuffle[0..Product.all.size]
     @products = []
-    Product.all.order("created_at ASC").each do |x|
+    Product.all.order("created_at DESC").each do |x|
       if x.published == true
         @products << x
       end

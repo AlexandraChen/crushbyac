192aa7065c92109c5a832a497bf35564daa68be2
Alexandra Chen <alechen04@gmail.com>
2015-11-10 15:19:17 -0500

    add product filters

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -2,5 +2,43 @@ $(document).ready(function () {
   $('#interested-product').click(function(){
     $('#contact-product').toggle()
   })
+
+  function search_query() {
+    var query = $('#search').val()
+    router.collection.fetch(query, function () {
+      router.ProductComponent.render()
+    })
+  }
+
+  $('#bikinis').click(function(event){
+    event.preventDefault()
+    $('#search').val("bikini")
+    search_query()
+  })
+
+  $('#all-products').click(function(event){
+    event.preventDefault()
+    $('#search').val("")
+    search_query()
+  })
+
+  $('#trikinis').click(function(event){
+    event.preventDefault()
+    $('#search').val("trikini")
+    search_query()
+  })
+
+  $('#accessories').click(function(event){
+    event.preventDefault()
+    $('#search').val("accessories")
+    search_query()
+  })
+
+  $('#clothing').click(function(event){
+    event.preventDefault()
+    $('#search').val("activewear")
+    search_query()
+  })
+
 })
 

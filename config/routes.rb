b626138ce25bb1bb26f7e76142447263cd6fe316
Alexandra Chen <alechen04@gmail.com>
2016-01-04 20:43:06 -0500

    Ordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,20 +7,18 @@ Rails.application.routes.draw do
     get "/logout", to: "devise/sessions#destroy", as: :signout
   end
 
-  get "/admin/panel" => "users#admin_panel"
-  resources :products
-
   namespace :api, defaults: { format: :json } do
     resources :products, only: [:index]
   end
 
-  resources :contact_forms, only: [:new, :create]
-
+  get "/admin/panel"          => "users#admin_panel"
   get "/contact"              => "sites#contact_us"
   get "/location"             => "sites#location"
   get "/faq"                  => "sites#faq"
   get "/terms_and_conditions" => "sites#terms_and_conditions"
-
+ 
+  resources :contact_forms, only: [:new, :create]
+  resources :products, except: [:destroy]
   resources :clients, only: [:new, :create, :show]
 
   get "/happy_customers"      => "clients#happy_customers"

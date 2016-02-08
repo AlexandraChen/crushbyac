a4290f77383be730e06a1720631fbcfc21e0d817
Alexandra Chen <alechen04@gmail.com>
2016-02-07 19:13:09 -0500

    Add user id to clients

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :products
-
+  has_many :colors
+  has_many :clients
+  
   validates :email, presence: true, format: { with: /.+@.+\..+/i }
   validates :full_name, presence: true, format: { with: /\A[^0-9`!@#\$%\^&*+_=]+\z/ }
  	validate :full_name_length

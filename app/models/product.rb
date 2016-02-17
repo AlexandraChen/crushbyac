cb8637bacbbfdc9a4cc24b019972626cd9ea0708
Alexandra Chen <alechen04@gmail.com>
2016-02-16 20:13:09 -0500

    Added uniquenes validation to fill and refactoring

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,12 +5,7 @@ class Product < ActiveRecord::Base
   validates :user, presence: true
   validates :price, presence: true
   validates :size, presence: true
-  has_attached_file :img,
-      :styles => { 
-        :medium => "450x450>",
-        :thumb => "100x100>"
-      },
-       :default_url => "no_pic.png"
+  has_attached_file :img, :styles => { :medium => "450x450>", :thumb => "100x100>" }, :default_url => "no_pic.png"
   validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :img
 
--- app/models/top_design.rb
+++ app/models/top_design.rb
@@ -1,10 +1,5 @@
 class TopDesign < ActiveRecord::Base
-  has_attached_file :img,
-      :styles => { 
-        :medium => "450x450>",
-        :thumb => "100x100>"
-      },
-       :default_url => "no_pic.png"
+  has_attached_file :img, :styles => { :medium => "450x450>", :thumb => "100x100>" }, :default_url => "no_pic.png"
   validates_attachment_content_type :img, :content_type => /\Aimage\/.*\Z/
   validates_attachment_presence :img
   validates :name, presence: true, uniqueness: true

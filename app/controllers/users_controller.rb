1e0441afc9d9d0c6fdf047edf6a6cfb186d528a2
Alexandra Chen <alechen04@gmail.com>
2016-02-18 21:40:33 -0500

    Change clients in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 		if current_user.admin?
 			@admin = current_user
       @products = @admin.products.order("updated_at DESC")
-      @clients = @admin.clients.order("updated_at DESC")
+      @clients = Client.all.order("updated_at DESC")
       @colors = @admin.fills.order("updated_at DESC").where(fill_type:"Color")
       @patterns = @admin.fills.order("updated_at DESC").where(fill_type:"Pattern")
       @top_designs = @admin.top_designs.order("updated_at DESC")

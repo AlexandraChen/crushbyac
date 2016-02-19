70d9e56f7af24b6514bc74e6c74e67d30e3d8731
Alexandra Chen <alechen04@gmail.com>
2016-02-18 20:47:20 -0500

    Delete ! from email

--- app/controllers/contact_forms_controller.rb
+++ app/controllers/contact_forms_controller.rb
@@ -6,7 +6,7 @@ class ContactFormsController < ApplicationController
   def create
     @contact_form = ContactForm.new(params[:contact_form])
     @contact_form.request = request
-    if @contact_form.deliver!
+    if @contact_form.deliver
       redirect_to :back, notice: I18n.t(:email_success)
     else
       redirect_to :back, alert: I18n.t(:fill_fields)

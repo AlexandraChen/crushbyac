0e7645822d8aaa6394afff8dfe0ec33bb93a3fca
Alexandra Chen <alechen04@gmail.com>
2016-05-16 18:13:00 -0500

    Styling to cart view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
     @contact_form = ContactForm.new
+    @order_item = current_order.order_items.new
   end
 
   def new
